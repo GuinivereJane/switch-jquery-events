5e835f3db8ca27a62fbf5b904145321e7f4d1eaa
Guin <guinifer.k@gmail.com>
2016-04-25 15:36:45 -0400

    The light switch works

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-
-
+$('document').ready(function() {
+  $('button').click( function() {
+    if ($(this).attr('class') === "on"){
+      $(this).attr("class", "off");
+      $('body').attr('class', 'dark');
+      $('.status').html('It is so dark in here');
+    }else{
+      $(this).attr("class", "on");
+      $('body').attr('class', 'light');
+      $('.status').html('It is so bright in here');
+    }
+  });
+});
